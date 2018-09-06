dc79bed21dd2aff38f0f9139c5994800419cf515
Jazib Ali <m.mianjazibali@gmail.com>
2018-09-06 20:03:56 +0500

    Home Page Final

---
+++ googledrive/Scripts/Login.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $("#loginbtn").click(function () {
+        var $userlogin = $("#userlogin").val();
+        var $userpassword = $("#userpassword").val();
+
+        if ((!$userlogin) || (!$userpassword)) {
+            alert("Error ! One or More Fields Are Empty");
+            return false;
+        }
+
+        var $data = { 'Login': $userlogin, 'Password': $userpassword };
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            url: '/User/ValidateUser',
+            data: JSON.stringify($data),
+            contentType: "application/json",
+            processdata: false,
+            success: function (result) {
+                if (result == "") {
+                    alert("Invalid Username Or Password");
+                    $('#userpassword').val("");
+                }
+                else {
+                    window.location.href = '/Home';
+                }
+            },
+            error: function () {
+                alert('Failed To Login ! Internal Error');
+            }
+        });
+
+    });
+});
